019c2da35b5e20cfe947cd15e435adebd1607909
Neeraj Singh Junior <NeerajSingh511997@gmail.com>
2021-12-07 03:42:39 +0530

    added week03 problems solutions

---
+++ temp.py
@@ -0,0 +1,42 @@
+'''
+Problem Description:
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import time
+
+
+## Main Working Function, here...
+
+
+
+def main():
+    try:
+        res = ""
+        print(res) if res else print("Empty!")
+        
+    except(Exception) as e:
+        print(f"Exception Traced: {e}")
+    
+    else:
+        print("Program Executed: Success")
+
+    finally:
+        print("Program Terminated!")
+
+        
+
+if __name__ == '__main__':
+    print("#------------ Code Start --------------#")
+    startTime = time.time()
+    main()
+    endTime = time.time()
+    print("Run Time:",endTime-startTime,"ms")
+    print("#------------ Code Stop ----------------#")
+    
