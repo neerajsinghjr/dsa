c3abc82cbcbce28204b1cfada4af1c5aa30fa386
Neeraj Singh Junior <NeerajSingh511997@gmail.com>
2021-12-31 10:28:10 +0530

    addded find nodes function inside binary tree

--- Algorithms/BinaryTreeExpl.py
+++ Algorithms/BinaryTreeExpl.py
@@ -17,22 +17,40 @@ from Helpers.mBinaryTree import *
 def main():
     try:
         tree = Tree()       # Init Tree
-        tree.add(1)
-        tree.add(2)
-        tree.add(3)
-        tree.add(4)
-        tree.add(5)
-        tree.add(6)
+        # tree.add(1)
+        # tree.add(2)
+        # tree.add(3)
+        # tree.add(4)
+        # tree.add(5)
+        # tree.add(6)
+        # tree.add(7)
+        # tree.add(8)
+
+        tree.add(45)
+        tree.add(10)
         tree.add(7)
-        tree.add(8)
-        print("\nPre-Order...")
-        tree.show("preorder")
-        print("\nIn-Order...")
-        tree.show("inorder")
-        print("\nPost-Order...")
-        tree.show("postorder")
+        tree.add(90)
+        tree.add(12)
+        tree.add(50)
+        tree.add(13)
+        tree.add(39)
+        tree.add(57)        
+
+        # print("Height: ",tree.height())
+        # print("Length:",len(tree))
         
-        print("end...")
+        # print("Finding...:",tree.find(51))
+
+        # tree.show()
+
+        # print("\nPre-Order...")
+        # tree.show("preorder")
+        # print("\nIn-Order...")
+        # tree.show("inorder")
+        # print("\nPost-Order...")
+        # tree.show("postorder")
+        
+        print("Program End Reached...")
             
     except(Exception) as e:
         print(f"Exception Traced : {e}")
--- Algorithms/Helpers/mBinaryTree.py
+++ Algorithms/Helpers/mBinaryTree.py
@@ -37,7 +37,7 @@ class Tree:
     len() : len of the tree;
     """
     def __len__(self):
-        pass    
+        return self.height()
 
     """
     getRoot() : get root node;
@@ -46,7 +46,7 @@ class Tree:
         return self.root
 
     """
-    add() : build tree;
+    add() : add nodes;
     """
     def add(self,data):
         if(self.root == None):
@@ -55,7 +55,7 @@ class Tree:
             self._add(self.root,data)
 
     """
-    _add() : add node to the tree
+    _add() : add method init;
     """
     def _add(self,root,data):
         if(data < root.data):            # Left Sub Tree
@@ -86,11 +86,11 @@ class Tree:
             return None
 
     """
-    _show() : show the tree in default style;
+    _show() : show method init;
     """
     def _show(self,root):
         if(root is not None):
-            print(root.data,end=", ")            # print;
+            print(root.data,end=", ")          # print;
             self._show(root.left)              # go left;
             self._show(root.right)             # go right;
         return
@@ -100,7 +100,7 @@ class Tree:
     """
     def preorder(self,root=None):
         if(root is not None):
-            print(root.data,end=", ")            # print;
+            print(root.data,end=", ")      # print;
             self.preorder(root.left)       # go left;
             self.preorder(root.right)      # go right;
         return
@@ -111,7 +111,7 @@ class Tree:
     def inorder(self,root=None):
         if(root is not None):
             self.inorder(root.left)            # go left;
-            print(root.data,end=", ")            # print;
+            print(root.data,end=", ")          # print;
             self.inorder(root.right)           # go right;
         return 
 
@@ -125,15 +125,53 @@ class Tree:
             print(root.data,end=", ")            # print;
         return
 
-    """
-    find() : find note inside tree;
-    """
-    def find(self,data):
-        pass
 
+    """
+    find() : find nodes inside tree;
+    """
+    def find(self, data):
+        if self.root is not None:
+            return self._find(self.root,data)
+        else:
+            return None
+
+    """
+    _find() : find method init;
+    """
+    def _find(self,root,data):
+        if(root is not None):
+            if data == root.data:
+                return f"Node {data}: Found"
+            elif (data < root.data):
+                return self._find(root.left,data)
+            elif (data > root.data):
+                return self._find(root.right,data)
+        else:
+            return f"Node {data}: Not Found"
+    
     """
     delete() : delete a not from tree;
     """
     def delete(self,data):
         pass
-    
+
+    """
+    height() : height of a tree
+    """
+    def height(self):
+        if(self.root is None):
+            return 0
+        else:
+            return self._height(self.root)
+
+    """
+    _height() : height method init;
+    """
+    def _height(self,root):
+        if(root == None):
+            return 0
+        else:
+            lenOne = self._height(root.left)
+            lenTwo = self._height(root.right)
+            return 1+max(lenOne,lenTwo)
+            
