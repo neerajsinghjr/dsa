9b51e94b5eef1f4db60a67105ebd5b77ccdd1f30
neerajsinghjr <neerajsingh511997@gmail.com>
2022-09-04 22:48:57 +0530

    update filename for BasicProgram

--- BasicProgm/Py_06_Multiplication_Table.py
+++
@@ -1,8 +0,0 @@
-## Program: Multiplication Table;
-try:
-    number = int(input("Your number: "))
-    for x in range(1, 11):
-        print("%d x %d = %d"%(number, x, number*x))
-
-except Exception as error:
-    print("Something went wrong. ", error)
