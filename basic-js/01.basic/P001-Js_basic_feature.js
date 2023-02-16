a2184e12e92bf491035b8bd2c1ef1d9f9a5daef0
Neeraj Singh Jr <NeerajSingh511997@gmail.com>
2023-02-16 16:44:02 +0530

    added js notes and rename dir name

--- basic-js/01.basic/P001-Js_basic_feature.js
+++ basic-js/01.basic/P001-Js_basic_feature.js
@@ -11,4 +11,55 @@
 */
 
 
-console.log("Hello World")
+
+// checking scope of variable;
+let a = 10
+console.log(a)
+if(a == 10) {
+	console.log(a)
+	a = 20
+	console.log(a)
+}
+console.log(a)
+
+
+//--- increment or decrement operator;
+// var number = 10
+// console.log(number)
+// number++
+// console.log(number)
+// number++
+// console.log(number)
+
+// var num = 1
+// console.log(num)
+// ++num
+// console.log(num)
+// ++num
+// console.log(num)
+
+
+
+// count = 20
+// message = "Hello World";
+
+// console.log(count)
+// console.log(typeof count)
+// console.log(message)
+// console.log(typeof message)
+
+// isAdmin = false && true
+// console.log(isAdmin)
+
+// const num1 = 1290
+// const num2 = -1290
+// const num3 = 12.90
+
+// console.log(`num1: ${num1} || num2 : ${num2} || num3: ${num3}`)
+
+// const personID = "123abc";
+// const quotient = personID / 2;
+// console.log(typeof quotient === NaN)
+// console.log(quotient);
+
+// console.log(isNaN(quotient))
