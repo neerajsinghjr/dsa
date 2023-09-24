363f32fc072300d72c6dfa666139406742bff889
Neeraj Singh Jr <NeerajSingh511997@gmail.com>
2023-09-24 11:58:48 +0530

    added string problem

---
+++ coding-ninjas/dsa-p1/03-strings/p001-string-titlecase.py
@@ -0,0 +1,73 @@
+'''
+-------------------------------------------------------------------------------------
+-> Problem Title: 
+-> Problem Status: Ongoing...
+-> Problem Attempted:
+-> Problem Description: 
+-------------------------------------------------------------------------------------
+
+...
+
+-------------------------------------------------------------------------------------
+'''
+
+#!/bin/python3
+
+import os
+import re
+import sys
+import time
+import math
+import random
+
+
+##---Main Solution
+def convertString(str):
+	"""
+    time: o(n)
+    space: o(1)
+    run: success
+    choke: none 
+    brief: simple iterate over the loop and check for
+    space and then upper the next character.
+    """
+    res, space_traced = '', True
+    for idx in range(len(str)):
+        if str[idx].isalpha() and space_traced:
+            space_traced = False
+            res += str[idx].upper()
+        elif str[idx] == ' ':
+            space_traced = True
+            res += str[idx]
+        else:
+            space_traced = False
+            res += str[idx]
+            
+    return res
+
+
+##---Main Execution;;
+def main(res=None):
+    try:
+    	data = []				
+        obj = Solution()	
+        res = None
+        print(f"Result: {res}") if res else print("Empty!")
+        
+    except(Exception) as e:
+        print(f"Exception Traced : {e}")
+    
+    else:
+        print("Program Completed : Success")
+
+    finally:    
+        print("Program Terminated!")
+
+
+if __name__ == '__main__':
+    print("#------------ Code Start --------------#")
+    startTime = time.time()
+    main()
+    endTime = time.time()
+    print("Run Time:",endTime-startTime,"ms")
+    print("#------------ Code Stop ----------------#")
