d45d96c4cf1a90074ebcecfdb8fa03a2b7177920
Neeraj Singh Jr <NeerajSingh511997@gmail.com>
2023-10-15 13:09:24 +0530

    added docker volumne code for first-app

--- docker-apps/first-app/app.py
+++ docker-apps/first-app/app.py
@@ -1,8 +1,30 @@
+import os
+import traceback
+from time import time
+from csv import writer
+
+
+def save(user_id, user_name):
+	file = f'user_log/file_{user_id}'
+	header = ['user_id', 'name']
+	mode = 'a' if os.path.exists(file) else 'w'
+	
+	try:
+		data = [user_id, user_name]
+		with open(file, mode) as fileobj:
+			csv_obj = writer(fileobj)
+			if mode == 'w':
+				csv_obj.writerow(header)
+			csv_obj.writerow(data)
+	except:
+		print("Error Occured: ", traceback.format_exc())
+
+
 def main():
-	print("Hello, I'm docker-first-app")
-	name = input("Your Name...")
-	print(f"Hi {name}")
-	print("Shutting down...")
+	print("Hi, Welcome")
+	user_id = f"user_{round(time())}"
+	user_name = input("Your Name: ")
+	save(user_id, user_name)
 
 
 if __name__ == "__main__":
